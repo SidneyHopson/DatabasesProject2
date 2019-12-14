a77bafe595573667f6e1b3a208561dbb5f13d51f
Sidney Hopson <sidneydhopson@gmail.com>
2019-12-14 16:54:35 -0500

    InitialUpdate

--- MarkLogic_C#/MarkLogic/Controllers/MarkLogicController.cs
+++ MarkLogic_C#/MarkLogic/Controllers/MarkLogicController.cs
@@ -12,29 +12,53 @@ namespace MarkLogic.Controllers
 {
     public class MarkLogicController : Controller
     {
-        public IActionResult Index()
+
+        public async Task<Stream> Browse(List<IFormFile> files, string start)
+        {
+            string pageLength = "100";
+            using (var client = new HttpClient())
+            {
+                var httpClientHandler = new HttpClientHandler()
+                {
+                    Credentials = new NetworkCredential("admin", "admin"),
+                };
+                var httpClient = new HttpClient(httpClientHandler);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                //httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue());
+                var result = await httpClient.GetStreamAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/search?start=" + start + "&pageLength=" + pageLength);
+                return result;
+            }
+            
+        }
+
+        public IActionResult Download()
         {
             return View();
         }
 
-        public async Task<string> Get()
+        public async Task<string> Get(IFormFile file, string uri)
         {
             using (var client = new HttpClient())
             {
                 var httpClientHandler = new HttpClientHandler()
                 {
-                    Credentials = new NetworkCredential("wenli", "WANG3838"),
+                    Credentials = new NetworkCredential("admin", "admin"),
                 };
                 var httpClient = new HttpClient(httpClientHandler);
                 httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var result = await httpClient.GetStringAsync("http://localhost:8000/LATEST/documents?uri=/example/test.json");
+               // httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue());
+                var result = await httpClient.GetStringAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/documents?uri=" + uri);
                 return result;
             }
         }
 
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
         [HttpPost]
-        public async Task<IActionResult> Upload( List<IFormFile> files)
+        public async Task<IActionResult> Upload( List<IFormFile> files, string uri)
         {
             long size = files.Sum(f => f.Length);
             foreach (var formFile in files)
@@ -47,19 +71,125 @@ namespace MarkLogic.Controllers
                     {
                         var httpClientHandler = new HttpClientHandler()
                         {
-                            Credentials = new NetworkCredential("wenli", "WANG3838"),
+                            Credentials = new NetworkCredential("admin", "admin"),
                         };
                         var httpClient = new HttpClient(httpClientHandler);
                         httpClient.DefaultRequestHeaders.Accept.Clear();
-                        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        await httpClient.PutAsync("http://localhost:8000/LATEST/documents?uri=/example/test.json", content);
+                        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(formFile.ContentType));
+                        await httpClient.PostAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/documents?uri=" + uri, content);
                     }
                 }
+                
             }
             return Ok(new { count = files.Count, size});
             // NuGet Newtonsoft.Json library for object conversion
             // string json = JsonConvert.SerializeObject(account, Formatting.Indented);
             // Account account_back = JsonConvert.DeserializeObject<Account>(json);
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(List<IFormFile> files, string uri)
+        {
+            long size = files.Sum(f => f.Length);
+            foreach (var form in files)
+            {
+                using (var client = new HttpClient())
+                {
+                    var httpClientHandler = new HttpClientHandler()
+                    {
+                        Credentials = new NetworkCredential("admin", "admin"),
+                    };
+                    var httpClient = new HttpClient();
+                    await httpClient.DeleteAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/documents?uri=" + uri);
+                }
+            }
+            return RedirectToAction(nameof(Browse));
+        }
+
+        public async Task<Stream> GetDoc(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                var httpClientHandler = new HttpClientHandler()
+                {
+                    Credentials = new NetworkCredential("admin", "admin"),
+                };
+                var httpClient = new HttpClient(httpClientHandler);
+                var result = await httpClient.GetStreamAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/documents?uri=" + uri);
+                //Response.Headers.Add("Content-Disposition", "filename=" + fileName);
+                //return File(data, result.GetType);
+                return result;
+            }
+        }
+
+        public async Task<Stream> StringQueryDoc(string uri, string start)
+        {
+            string length = "100";
+            using (var client = new HttpClient())
+            {
+                var httpClientHandler = new HttpClientHandler()
+                {
+                    Credentials = new NetworkCredential("admin", "admin"),
+                };
+                var httpClient = new HttpClient(httpClientHandler);
+                var result = await httpClient.GetStreamAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/search?q=" + uri +"&start=" + start +"&pageLength=" + length);
+
+                
+                // Retrieving the confidence and fitness levels through the received XML document.
+                //doc.Confidence = Convert.ToDouble(node.Attributes["confidence"].InnerXml);
+                //doc.Fitness = Convert.ToDouble(node.Attributes["fitness"].InnerXml);
+
+                return result;
+            }
+        }
+
+        public async Task<Stream> StructuredQueryDoc(string uri, string start)
+        {
+            string length = "100";
+            using (var client = new HttpClient())
+            {
+                var httpClientHandler = new HttpClientHandler()
+                {
+                    Credentials = new NetworkCredential("admin", "admin"),
+                };
+                var httpClient = new HttpClient(httpClientHandler);
+                var result = await httpClient.GetStreamAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/search?structuredQuery=" + uri + "&start=" + start + "&pageLength=" + length);
+
+
+                // Retrieving the confidence and fitness levels through the received XML document.
+                //doc.Confidence = Convert.ToDouble(node.Attributes["confidence"].InnerXml);
+                //doc.Fitness = Convert.ToDouble(node.Attributes["fitness"].InnerXml);
+
+                return result;
+            }
+        }
+
+
+        public async Task<IActionResult> QBEQueryDoc(string uri, string length)
+        {
+            using (var client = new HttpClient())
+            {
+                var httpClientHandler = new HttpClientHandler()
+                {
+                    Credentials = new NetworkCredential("admin", "admin"),
+                };
+                var httpClient = new HttpClient(httpClientHandler);
+                var result = await httpClient.GetAsync("http://e8instructsql2.ad.psu.edu:8000/LATEST/qbe?query=" + uri + "&pageLength=" + length);
+
+
+                // Retrieving the confidence and fitness levels through the received XML document.
+                //doc.Confidence = Convert.ToDouble(node.Attributes["confidence"].InnerXml);
+                //doc.Fitness = Convert.ToDouble(node.Attributes["fitness"].InnerXml);
+
+                return View(result);
+            }
+            
         }
     }
 }
--- MarkLogic_C#/MarkLogic/Startup.cs
+++ MarkLogic_C#/MarkLogic/Startup.cs
@@ -45,7 +45,7 @@ namespace MarkLogic
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=MarkLogic}/{action=Upload}/{id?}");
             });
         }
     }
